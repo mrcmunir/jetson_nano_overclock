4b5132189534456a12d3498a53cfb3bd589a52ac
Carlos Estrague <maximu@gmail.com>
2020-07-25 14:08:06 +0200

    Enable CPU speed up o 2,014ghz instean 1,4ghz and remove limit GPU core so up now to 1,0ghz

---
+++ kernel/kernel-4.9/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Jul 24 20:00:46 EDT 2020"
+#define LINUX_COMPILE_BY "aresuser"
+#define LINUX_COMPILE_HOST "aresuser-desktop"
+#define LINUX_COMPILER "gcc version 7.5.0 (Ubuntu/Linaro 7.5.0-3ubuntu1~18.04) "
---
+++ nvcommon_build.sh
@@ -0,0 +1,46 @@
+#
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+#
+
+# Function to check if variable is defined.
+function check_vars {
+	variables=${@}
+	for variable in ${variables} ; do
+		if [ -z "${!variable}" ]; then
+			echo "Error: Env variable ${variable} is not set!!"
+			exit 1
+		fi
+	done
+}
+
+# Function to check build environment
+function check_env_common {
+	if [ "${IS_CROSS_COMPILATION}" -eq 1 ]; then
+		check_vars "CROSS_COMPILE_AARCH64_PATH"
+		CROSS_COMPILE_AARCH64="${CROSS_COMPILE_AARCH64_PATH}/bin/aarch64-linux-gnu-"
+		if [ ! -f "${CROSS_COMPILE_AARCH64}gcc" ]; then
+			echo "Error: Path ${CROSS_COMPILE_AARCH64}gcc does not exist."
+			exit 1
+		fi
+	fi
+	if [ "${MAKE_BIN}" != "make" ] && \
+		( [ ! -f "${MAKE_BIN}" ] || \
+		[ ! -f $(basename "${MAKE_BIN}") ] ); then
+		echo "Error: ${MAKE_BIN} does not exist !!"
+		echo "Set MAKE_BIN env variable to "make" binary"
+		exit 1
+	fi
+}
+
+
+NPROC=$(nproc)
+MAKE_BIN="${MAKE_BIN:-make}"
+BUILD_DIR="${BUILD_DIR:-$(pwd)/build_nv_sources}"
+
+MACHINE=$(uname -m)
+
+IS_CROSS_COMPILATION=0
+if [[ "${MACHINE}" =~ "x86" ]]; then
+	IS_CROSS_COMPILATION=1
+fi
+check_env_common
