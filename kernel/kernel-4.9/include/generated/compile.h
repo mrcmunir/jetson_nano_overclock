9210eb407a8d01cef3d1ee7adac5cfe5f769d66c
Carlos Estrague <maximu@gmail.com>
2025-03-17 23:06:05 +0100

    Update cleanup files

--- kernel/kernel-4.9/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 3 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#3 SMP PREEMPT Thu Dec 28 19:52:57 CET 2023"
-#define LINUX_COMPILE_BY "aresuser"
-#define LINUX_COMPILE_HOST "aresuser-desktop"
-#define LINUX_COMPILER "gcc version 11.4.0 (Ubuntu 11.4.0-1ubuntu1~22.04) "
